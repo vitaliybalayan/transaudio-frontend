2f3edf7c910e2ba98753fdc023048082cda2cc84
vitaliybalayan <vitaliy.balayan@gmail.com>
2025-09-15 02:32:00 +0500

    feat: backend url in .env

--- src/libs/apollo-client.ts
+++ src/libs/apollo-client.ts
@@ -1,20 +1,12 @@
 import { InMemoryCache } from '@apollo/client';
 import { ApolloClient } from '@apollo/client';
-// import { SERVER_URL } from './constants/url.constants';
+import { SERVER_URL } from './constants/url.constants';
 // @ts-ignore
 import UploadHttpLink from 'apollo-upload-client/UploadHttpLink.mjs';
 
-// const httpLink = createUploadLink({
-// 	uri: SERVER_URL,
-// 	credentials: 'include',
-// 	headers: {
-// 		'apollo-require-preflight': 'true'
-// 	}
-// })
-
 const httpLink = new UploadHttpLink({
 	// todo: Перенести в .env
-	uri: 'http://localhost:4000/graphql',
+	uri: SERVER_URL,
 	credentials: 'include',
 	headers: {
 		'apollo-require-preflight': 'true',
